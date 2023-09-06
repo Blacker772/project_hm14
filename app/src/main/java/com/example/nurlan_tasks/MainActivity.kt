07099b482dfd12ec0897ea09de9479757f2ead62
NurlanVeliev <nurlan.veliev@list.ru>
2023-09-06 08:40:23 +0400

    First commit

---
+++ app/src/main/java/com/example/nurlan_tasks/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nurlan_tasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ui_hm14_task1_nurlanveliev)
+    }
+}
